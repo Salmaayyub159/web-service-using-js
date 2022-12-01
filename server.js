e287fefc92018ea9c6caeb6c0e43864e5f673d1d
Salmay <salmaayyub01@gmail.com>
2022-12-01 17:40:54 +0800

    buat http server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello, this is my first project built web service using Js");
+const http = require('http');
+ 
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+ 
+    response.statusCode = 200;
+    response.end('<h1>Halo HTTP Server!</h1>');
+};
+ 
+ 
+const server = http.createServer(requestListener);
+ 
+const port = 5000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
